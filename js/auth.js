03cd69d60b05d93f278d9bce6ede48d945257ce5
akandss <anitakdr79@gmail.com>
2025-12-24 02:22:22 -0800

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,18 @@
+const PASSWORD = "onalaksa";
+
+function login() {
+  const input = document.getElementById("password").value;
+
+  if (input === PASSWORD) {
+    localStorage.setItem("access", "granted");
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("error").innerText = "Not quite 🤍";
+  }
+}
+
+function checkAccess() {
+  if (localStorage.getItem("access") !== "granted") {
+    window.location.href = "login.html";
+  }
+}
